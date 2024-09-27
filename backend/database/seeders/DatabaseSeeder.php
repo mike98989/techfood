657a6a4e1095e622ed39a59ac3a2e4ea4ad1fb94
michael <mike98989@gmail.com>
2024-09-27 20:06:14 +0200

    Minor update

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //LabInputs::factory(100)->create();
+        LabInputs::factory(100)->create();
     }
 }
